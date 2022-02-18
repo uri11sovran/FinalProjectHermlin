1ee6c85c2f0a1179656622f563a553bc44942dd0
URI sovran <uri1sovran@gmail.com>
2022-02-18 16:44:34 +0200

    changed so every screen with deferent quality can still view the game images the same

--- src/GUI/ImageHandler.java
+++ src/GUI/ImageHandler.java
@@ -1,6 +1,8 @@
 package GUI;
 
+import java.awt.DisplayMode;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Toolkit;
 
@@ -9,12 +11,25 @@ import javax.swing.JFrame;
 import logic.PlayerType;
 
 public class ImageHandler {
+	public static final double SCREEN_HIGHT_TO_IMG_SIZE_RATIO = 0.00462962963;
+	public static final double SCREEN_WIDTH_TO_IMG_SIZE_RATIO = 0.00260416;
+	private static final int IMG_LEN = 20;
+	
 	public static void paint(Graphics gr, int x, int y, PlayerType player_type, JFrame frame) 
 	{
+		DisplayMode mode = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
+		
 		Image img = Toolkit.getDefaultToolkit().getImage(ImageHandler.getPieceImage(player_type));
-		y += 20;
-		gr.drawImage(img, x - 10, y - 10, 20, 20, frame);
-		y -= 20;
+		
+		// moves the image down by one image frame
+		y += IMG_LEN;
+		
+		// draws image
+		gr.drawImage(img,
+					x - (int)(Math.round(mode.getWidth() * SCREEN_WIDTH_TO_IMG_SIZE_RATIO)),
+					y - (int)(Math.round(mode.getHeight() * SCREEN_HIGHT_TO_IMG_SIZE_RATIO)),
+					IMG_LEN, IMG_LEN,
+					frame);
 	}
 	
 	public static String getPieceImage(PlayerType player_turn)
